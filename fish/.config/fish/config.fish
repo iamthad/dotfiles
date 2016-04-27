738da3a530c425d0a63b4134fa76be0ce29a6a88
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2016-04-26 20:02:24 -0500

    Add homebrew-command-not-found to fish.config and check OS

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
-if test -f ~/.iterm2_shell_integration.fish
-  source ~/.iterm2_shell_integration.fish
-end
+switch (uname)
+    case Darwin
+        if test -f ~/.iterm2_shell_integration.fish
+          source ~/.iterm2_shell_integration.fish
+        end
+        # Initialize homebrew-command-not-found
+        brew command command-not-found-init > /dev/null; and . (brew command-not-found-init)
