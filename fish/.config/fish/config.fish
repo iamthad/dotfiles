53a3a1fde1c1bfc9064a58c4ac3748e5d40cc348
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2016-09-28 08:15:44 -0500

    Clean up Mac-specific config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,11 @@
 switch (uname)
     case Darwin
+    # Mac-specific setup
         if test -f ~/.iterm2_shell_integration.fish
           source ~/.iterm2_shell_integration.fish
         end
         # Initialize homebrew-command-not-found
-        brew command command-not-found-init > /dev/null; and . (brew command-not-found-init)
+        if brew command command-not-found-init ^/dev/null >/dev/null
+          source (brew command-not-found-init)
+        end
 end
-eval (python -m virtualfish auto_activation)
-status --is-interactive; and . (rbenv init -|psub)
