fa6ad5a3c0272947a78d12ea40c339690c41517d
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2019-03-04 09:10:42 -0600

    Fix set error on bash 5

--- bash/.config/bash/rc.d/60-direnv.sh
+++ bash/.config/bash/rc.d/60-direnv.sh
@@ -1,6 +1,8 @@
+oldopts="$-"
 # Enable direnv if present
-set -h -- "-${-:--}" "$@"
+set -h
 if hash direnv 2>/dev/null; then
     eval "$(direnv hook bash)"
 fi
-set +h "$@"
+[[ "$oldopts" =~ "h" ]] || set +h
+unset oldopts
